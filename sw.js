300d2694c0534695ba476d590a8877e5a735d70f
SenzaPioF <salernopio99@gmail.com>
2026-09-07 14:29:58 +0200

    Update cache version to 'jarvis-mk85-v6'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jarvis-mk85-v5';
+const CACHE = 'jarvis-mk85-v6';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
