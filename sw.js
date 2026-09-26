ca983780554cffbca5ca0bb3502967db936c9258
SenzaPioF <salernopio99@gmail.com>
2026-09-26 17:36:24 +0200

    Update cache version from v12 to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jarvis-mk85-v12';
+const CACHE = 'jarvis-mk85-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
