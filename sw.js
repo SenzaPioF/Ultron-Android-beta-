01bbae8a07b34248efc0ee6b2dd9036a037dcfa9
SenzaPioF <salernopio99@gmail.com>
2026-09-26 18:00:51 +0200

    Update cache version from jarvis-mk85-v13 to nebula-v14

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jarvis-mk85-v13';
+const CACHE = 'nebula-v14';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
